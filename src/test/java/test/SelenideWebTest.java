03ef59d91103f5326e183d903625c5d63766e807
Andrei Papruha <andreipopruha@gmail.com>
2023-01-17 10:56:18 +0400

    ForReview

--- src/test/java/test/SelenideWebTest.java
+++ src/test/java/test/SelenideWebTest.java
@@ -5,26 +5,64 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 
 public class SelenideWebTest {
+    private static final String BASE_URL = "https://www.jetbrains.com/";
+    private static final String LANGUAGE_BUTTON_LOCATOR = "button[data-test=language-picker]";
+    private static final String LANGUAGE_GRID_LOCATOR = "div[data-test=language-grid] [data-test=list-item]";
+    private static final String HEADER_LOCATOR = "div#head-section>section>div>div>div>h1";
+    private static final String FOOTER_LOCATOR = "div#footer-container>footer>div";
+    private static final String INVENTORY_LOCATOR = "div#head-section>section>div>div:nth-of-type(4)>div:nth-of-type(2)>ul";
 
     @BeforeEach
-    void precondition() {
-        Selenide.open("https://www.jetbrains.com/");
+    void preliminaryOpeningOfSite() {
+        Selenide.open(BASE_URL);
     }
 
+    @ValueSource(strings = {
+            "JavaScript",
+            ".NET",
+            "Java & JVM",
+            "C++",
+            "macOS & iOS",
+            "Python & Django",
+            "PHP",
+            "Ruby & Rails",
+            "Go",
+            "Kotlin",
+            "SQL",
+            "See all tools"})
+    @ParameterizedTest(name = "Отображение кнопок{0} в навигационном меню шапки сайта")
+    @Tag("Critical")
+    void testingPresenceButtonsInNavigationMenu(String inventory) {
+        $(INVENTORY_LOCATOR).shouldHave(text(inventory));
+    }
 
-    static Stream<Arguments> selenideButtonsTest() {
+    @CsvSource(value = {"" +
+            "Deutsch, Unentbehrliche Tools für Software-Entwickler*innen und Teams",
+            "English, Essential tools for software developers and teams"
+    })
+    @ParameterizedTest(name = "Проверка наличия текста ({1}) в головной секции для выбранного языка {0}")
+    @Tag("Major")
+    void checkTextInHeader(String locale, String text) {
+        $(LANGUAGE_BUTTON_LOCATOR).click();
+        $$(LANGUAGE_GRID_LOCATOR).find(text(locale)).click();
+        $(HEADER_LOCATOR).shouldHave(text(text));
+    }
+
+    static Stream<Arguments> checkingCorrectNameButtonsInFooterSiteForDifferentLanguages() {
         return Stream.of(
                 Arguments.of("English", List.of("Privacy & Security", "Terms of Use", "Trademarks", "Legal", "Genuine Tools")),
                 Arguments.of("Deutsch", List.of("Impressum", "Datenschutz und Sicherheit", "Nutzungsbedingungen", "Trademarks", "Rechtliches", "Originalsoftware")),
@@ -34,13 +72,13 @@ public class SelenideWebTest {
     }
 
     @MethodSource
-    @ParameterizedTest(name = "Проверка наличия кнопок из списка {1} на сайте селенида в локали {0}")
+    @ParameterizedTest(name = "Проверка коректного названия кнопок из списка {1} на сайте jetBrains в локали {0}")
     @Tag("Minor")
-    void selenideButtonsTest(String locale, List<String> buttons) {
-        $("button[data-test=language-picker]").click();
-        $$("div[data-test=language-grid] [data-test=list-item]").find(text(locale)).click();
-        $$("footer[data-test=footer]>div>div:nth-of-type(2)>div:nth-of-type(3)>div a").shouldHave(texts(buttons));
+    void checkingCorrectNameButtonsInFooterSiteForDifferentLanguages(String locale, List<String> buttons) {
+        $(LANGUAGE_BUTTON_LOCATOR).click();
+        $$(LANGUAGE_GRID_LOCATOR).find(text(locale)).click();
+        for (String button : buttons) {
+            $$(FOOTER_LOCATOR).find(text(button)).shouldBe(visible);
+        }
     }
-
-
 }
